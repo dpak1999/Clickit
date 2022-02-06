8eff4a62ae63488716568d661c342059bf79c681
deepak <deepakkumardash313@gmail.com>
2022-02-06 23:04:10 +0530

    update profile page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,7 +1,134 @@
 /** @format */
 
+import { useEffect, useState } from 'react';
+import { GoogleLogout } from 'react-google-login';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useNavigate, useParams } from 'react-router-dom';
+import { client } from '../client';
+import {
+  userCreatedPinsQuery,
+  userQuery,
+  userSavedPinsQuery,
+} from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+const randomImage =
+  'https://source.unsplash.com/1600x900/?nature,photography,technology';
+
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [text, setText] = useState('Created');
+  const [activeBtn, setActiveBtn] = useState('created');
+
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  const logout = () => {
+    localStorage.clear();
+    navigate('/login');
+  };
+
+  useEffect(() => {
+    const query = userQuery(userId);
+
+    client.fetch(query).then((data) => setUser(data[0]));
+  }, [userId]);
+
+  useEffect(() => {
+    if (text === 'Created') {
+      const createdPinsQuery = userCreatedPinsQuery(userId);
+      client.fetch(createdPinsQuery).then((data) => setPins(data));
+    } else {
+      const savedPinsQuery = userSavedPinsQuery(userId);
+      client.fetch(savedPinsQuery).then((data) => setPins(data));
+    }
+  }, [text, userId]);
+
+  if (!user) return <Spinner message="Loading profile" />;
+
+  return (
+    <div className="relative pb-2 h-full justify-center items-center">
+      <div className="flex flex-col pb-5">
+        <div className="relative flex flex-col mb-7">
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src={randomImage}
+              alt="..."
+              className="w-full h-370 2xl:h-510 shadow-lg object-cover"
+            />
+            <img
+              src={user?.image}
+              alt="..."
+              className="rounded-full w-20 h-20 -mt-10 shadow-xl object-cover"
+            />
+            <h1 className="font-bold text-3xl text-center mt-3">
+              {user.username}
+            </h1>
+            <div className="absolute top-0 z-10 right-0 p-2">
+              {userId === user._id && (
+                <GoogleLogout
+                  clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+                  render={(renderProps) => (
+                    <button
+                      type="button"
+                      className="bg-white p-2 rounded-full shadow-md cursor-pointer outline-none"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      <AiOutlineLogout color="red" fontSize={21} />
+                    </button>
+                  )}
+                  onLogoutSuccess={logout}
+                  cookiePolicy="single_host_origin"
+                />
+              )}
+            </div>
+          </div>
+          <div className="text-center mb-7">
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent);
+                setActiveBtn('created');
+              }}
+              className={`font-bold p-2 rounded-full w-20 outline-none ${
+                activeBtn === 'created'
+                  ? 'bg-red-500 text-white'
+                  : 'bg-primary mr-4 text-black'
+              }`}
+            >
+              Created
+            </button>
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent);
+                setActiveBtn('saved');
+              }}
+              className={`font-bold p-2 rounded-full w-20 outline-none ${
+                activeBtn === 'saved'
+                  ? 'bg-red-500 text-white'
+                  : 'bg-primary mr-4 text-black'
+              }`}
+            >
+              Saved
+            </button>
+          </div>
+          {pins?.length ? (
+            <div className="px-2">
+              <MasonryLayout pins={pins} />
+            </div>
+          ) : (
+            <div className="flex justify-center font-bold items-center w-full text-xl mt-2">
+              No pins found
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
