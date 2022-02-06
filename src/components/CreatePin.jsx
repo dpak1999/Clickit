af9dd5e70569b62c6a30b2cd3714e4e6252b68a5
deepak <deepakkumardash313@gmail.com>
2022-02-06 20:30:22 +0530

    add : create pin component

--- src/components/CreatePin.jsx
+++ src/components/CreatePin.jsx
@@ -45,6 +45,35 @@ const CreatePin = ({ user }) => {
     }
   };
 
+  const savePin = () => {
+    if (title && about && destination && imageAsset?._id && category) {
+      const doc = {
+        _type: 'pin',
+        title,
+        about,
+        destination,
+        image: {
+          _type: 'image',
+          asset: {
+            _type: 'reference',
+            _ref: imageAsset?._id,
+          },
+        },
+        userId: user?._id,
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user?._id,
+        },
+        category,
+      };
+
+      client.create(doc).then(() => navigate('/'));
+    } else {
+      setFields(true);
+      setTimeout(() => setFields(false), 2000);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
       {fields && (
@@ -140,7 +169,24 @@ const CreatePin = ({ user }) => {
                 <option className="bg-white" value="other">
                   Select category
                 </option>
+                {categories.map((item) => (
+                  <option
+                    className="text-base border-0 outline-none capitalize bg-white text-black"
+                    value={item.name}
+                  >
+                    {item.name}
+                  </option>
+                ))}
               </select>
+            </div>
+            <div className="flex justify-end items-end mt-5">
+              <button
+                type="button"
+                onClick={savePin}
+                className="bg-red-500 text-white font-bold p-2 rounded-full outline-none w-28"
+              >
+                Save Pin
+              </button>
             </div>
           </div>
         </div>
