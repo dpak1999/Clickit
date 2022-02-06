94a6cd517e087c99b433a0359c5089a7ff5c9da7
deepak <deepakkumardash313@gmail.com>
2022-02-06 23:18:59 +0530

    fix search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,43 @@
 /** @format */
 
-const Search = () => {
-  return <div>Search Search</div>;
+import { useEffect, useState } from 'react';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+import { feedQuery, searchQuery } from '../utils/data';
+
+const Search = ({ searchterm }) => {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchterm) {
+      setLoading(true);
+      const query = searchQuery(searchterm.toLowerCase());
+      client.fetch(query).then((data) => {
+        console.log(data);
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchterm]);
+
+  return (
+    <div>
+      {loading && (
+        <Spinner message="Hang tight while we search & bring your posts" />
+      )}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchterm !== '' && !loading && (
+        <div className="mt-10 text-center">No posts found</div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
